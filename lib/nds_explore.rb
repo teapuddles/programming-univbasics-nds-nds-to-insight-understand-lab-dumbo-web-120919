80b89ffd06ce575b27b19c7d52eba18fc284d476
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-05 20:52:58 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,8 @@ def print_first_directors_movie_titles
  first = directors_database[0]
  i = 0
  while i < first[:movies].length do
-   
+   movie = first[:movies][i]
+   puts movie[:title]
    i += 1 
  end
 end
